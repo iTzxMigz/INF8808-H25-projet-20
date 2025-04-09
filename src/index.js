8e8ce4ff6e7e3b5528545e431548783602a87744
esteban <etebandectot@gmail.com>
2025-04-09 15:26:28 -0400

    design début

--- src/index.js
+++ src/index.js
@@ -13,74 +13,77 @@ import * as stackDotPlot from './scripts/stacked-dot-plot.js'
  * @version v1.0.0
  */
 
+/**
+ * @param numberOfGraph
+ */
 (function (d3) {
-  d3.csv('/netflix.csv', d3.autoType).then(function(data) {
-    let movies = preproc.getMoviesAndSeries(data);
-    let filterMoviesSeriesByYear = preproc.getFilterMoviesSeriesByYear(movies);
-    let chartData = Array.from(filterMoviesSeriesByYear, ([year, values]) => ({
+  d3.csv('/netflix.csv', d3.autoType).then(function (data) {
+    const movies = preproc.getMoviesAndSeries(data)
+    const filterMoviesSeriesByYear = preproc.getFilterMoviesSeriesByYear(movies)
+    const chartData = Array.from(filterMoviesSeriesByYear, ([year, values]) => ({
       year: +year,
       Movies: values.Movies,
       TVShows: values.TVShows
-    })).sort((a, b) => a.year - b.year);
-    drawShape(5);
-    stackBarChart.drawStackedBarChart(chartData);
-    let resultHeatmap = preproc.processCategoriesForHeatmap(movies, filterMoviesSeriesByYear);
-    let heatmapData = resultHeatmap.heatmapData
-    let categoriesList = resultHeatmap.sortedCategories
-    let dataStackedDotPlot = preproc.prepareStackedDotPlotData(movies);
-    heatmap.drawHeatmap(heatmapData);
-    let radarObject = preproc.preprocessRadarChart(movies, categoriesList);
-    let dataRadarChartAge = preproc.prepareRadarChartData(radarObject.radarAgeCert, "ageCert");
-    let dataRadarChartCat = preproc.prepareRadarChartData(radarObject.radarCategories, "");
-    radarChart.createChangingModeButton(dataRadarChartAge, dataRadarChartCat);
-    radarChart.drawMultipleRadarCharts(dataRadarChartAge, true);
-    geomap.drawGeomap(movies);
-    stackDotPlot.initDropdownAndPlot(dataStackedDotPlot.categories, dataStackedDotPlot.data);
-    changingTitleDynamically();
-  });
-
-
+    })).sort((a, b) => a.year - b.year)
+    drawShape(5)
+    stackBarChart.drawStackedBarChart(chartData)
+    const resultHeatmap = preproc.processCategoriesForHeatmap(movies, filterMoviesSeriesByYear)
+    const heatmapData = resultHeatmap.heatmapData
+    const categoriesList = resultHeatmap.sortedCategories
+    const dataStackedDotPlot = preproc.prepareStackedDotPlotData(movies)
+    heatmap.drawHeatmap(heatmapData)
+    const radarObject = preproc.preprocessRadarChart(movies, categoriesList)
+    const dataRadarChartAge = preproc.prepareRadarChartData(radarObject.radarAgeCert, 'ageCert')
+    const dataRadarChartCat = preproc.prepareRadarChartData(radarObject.radarCategories, '')
+    radarChart.createChangingModeButton(dataRadarChartAge, dataRadarChartCat)
+    radarChart.drawMultipleRadarCharts(dataRadarChartAge, true)
+    geomap.drawGeomap(movies)
+    stackDotPlot.initDropdownAndPlot(dataStackedDotPlot.categories, dataStackedDotPlot.data)
+    changingTitleDynamically()
+  })
 })(d3)
 
-function drawShape(numberOfGraph) {
-   for (let i = 0; i < numberOfGraph; i++) {
-      const div = d3.select("main")
-         .append("div")
-            .attr("class", "viz-container")
-            .attr("id", `viz-container-${i + 1}`);
-      div.append("div")
-         .attr("class", "graph")
-         .attr("id", `graph-${i + 1}`)
-   }
+function drawShape (numberOfGraph) {
+  for (let i = 0; i < numberOfGraph; i++) {
+    const div = d3.select('#cinema-screen')
+      .append('div')
+      .attr('class', 'viz-container')
+      .attr('id', `viz-container-${i + 1}`)
+    div.append('div')
+      .attr('class', 'graph')
+      .attr('id', `graph-${i + 1}`)
+  }
 }
 
-function changingTitleDynamically() {
-  const headerTitle = d3.select("header h1");
+/**
+ *
+ */
+function changingTitleDynamically () {
+  const headerTitle = d3.select('header h1')
 
-let titlesByViz = {
-  "viz-container-1": "Movies and Series added to Netflix by year",
-  "viz-container-2": "Distribution of Categories per year on Netflix",
-  "viz-container-3": () => radarChart.isAgeCertMode? radarChart.titleAgerCert : radarChart.titleCategories,
-  "viz-container-4": "Content Production and IMDb Scores by Country on Netflix",
-  "viz-container-5": () => stackDotPlot.isIMDB? stackDotPlot.titleIMDB : stackDotPlot.titleAge,
+  const titlesByViz = {
+    'viz-container-1': 'Movies and Series added to Netflix by year',
+    'viz-container-2': 'Distribution of Categories per year on Netflix',
+    'viz-container-3': () => radarChart.isAgeCertMode ? radarChart.titleAgerCert : radarChart.titleCategories,
+    'viz-container-4': 'Content Production and IMDb Scores by Country on Netflix',
+    'viz-container-5': () => stackDotPlot.isIMDB ? stackDotPlot.titleIMDB : stackDotPlot.titleAge
   // ajoute d'autres si tu veux
-};
+  }
 
-window.addEventListener("scroll", () => {
-  d3.selectAll(".viz-container").each(function() {
-    const rect = this.getBoundingClientRect();
-    const height = window.innerHeight
+  window.addEventListener('scroll', () => {
+    d3.selectAll('.viz-container').each(function () {
+      const rect = this.getBoundingClientRect()
+      const height = window.innerHeight
 
-    if (rect.top < height / 4 && rect.bottom > height - height / 4) {
-      const id = this.id;
-      const title = typeof titlesByViz[id] === "function"
-      ? titlesByViz[id]()
-      : titlesByViz[id];
-      if (title) {
-        headerTitle.text(title);
+      if (rect.top < height / 4 && rect.bottom > height - height / 4) {
+        const id = this.id
+        const title = typeof titlesByViz[id] === 'function'
+          ? titlesByViz[id]()
+          : titlesByViz[id]
+        if (title) {
+          headerTitle.text(title)
+        }
       }
-    }
-  });
-});
-
+    })
+  })
 }
