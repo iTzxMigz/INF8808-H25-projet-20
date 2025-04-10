9d13040a26bf3881a6bdb7be1264e2523099d517
esteban <etebandectot@gmail.com>
2025-04-10 12:41:52 -0400

    add scroll down button

--- src/index.js
+++ src/index.js
@@ -67,6 +67,7 @@ function callFunction2 () {
  */
 function callFunction3 () {
   hideTooltip()
+  d3.select('#scroll-down-animation').style('display', 'block')
   d3.select('.info-container p').text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
   d3.select('.bottom-bar h1').text('Distribution of Age Ratings by Country on Netflix')
   const container = d3.select('#button-container')
@@ -89,6 +90,8 @@ function callFunction4 () {
   d3.select('#table-container').classed('active', true)
   geomap.drawGeomap(movies)
 
+  // Masquer l'élément scroll-down-animation
+  d3.select('#scroll-down-animation').style('display', 'none')
   // Empêcher la propagation du scroll global lorsque l'utilisateur interagit avec le tableau
   const tableContainer = document.getElementById('table-container')
   const stopScrollPropagation = function (event) {
